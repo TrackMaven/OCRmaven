9df886992f92d27cf6cb457ec6c63617582fef55
Fletcher Heisler <fheisler@gmail.com>
2015-12-18 22:32:43 -0500

    Correct conf parsing

--- ocrmaven/ocr.py
+++ ocrmaven/ocr.py
@@ -1,5 +1,5 @@
 import requests
-from PIL import Image, ImageFilter
+from PIL import Image
 from StringIO import StringIO
 import pytesseract
 from bs4 import BeautifulSoup
@@ -7,7 +7,6 @@ from bs4 import BeautifulSoup
 
 def process_image(url):
     image = _get_image(url)
-    # image.filter(ImageFilter.SHARPEN)
     try:
         html = pytesseract.image_to_string(image, config="-c tessedit_create_hocr=1")
         text = _parse_html(html, conf_threshold=80)
@@ -25,7 +24,7 @@ def _parse_html(html, conf_threshold=80):
     text = u" "
     soup = BeautifulSoup(html, "html5lib")
     for span in soup.find_all('span', class_='ocrx_word'):
-        conf = span.attrs['title'].split()[-1]
+        conf = int(span.attrs['title'].split()[-1])
         if conf > conf_threshold:
             text += span.text.strip() + " "
     text = text.replace(u'\u2018', "'").replace(u'\u2019', "'")[:-1]
